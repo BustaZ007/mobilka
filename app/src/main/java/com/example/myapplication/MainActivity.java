916f21ea5983dedd9057bf053400de18e0b0d972
Pavel <pavel.zaborin@inbox.ru>
2020-04-28 19:42:54 +0500

    Add second activity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -22,46 +22,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    public void onClickDeleteText(View view) {
-
-        TextView textView = findViewById(R.id.hello);
-        textView.setText("PuPa");
-        textView.setTextColor(getResources().getColor(R.color.pupa));
-        textView.setBackgroundColor(getResources().getColor(R.color.lupa));
-    }
-
-    public void onClickWriteText(View view) {
-        TextView textView = findViewById(R.id.hello);
-        textView.setText("LuPa");
-        textView.setTextColor(getResources().getColor(R.color.lupa));
-        textView.setBackgroundColor(getResources().getColor(R.color.pupa));
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.line1:
-                Toast.makeText(this, "Не тычь на инвалида", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.line2:
-                Toast.makeText(this, "На него можно тыкать", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.line3:
-                Snackbar.make(findViewById(R.id.hello), "Сюда-то ты вообще зачем залез?!", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            default:
-                Dialog1 dialog = new Dialog1();
-                dialog.show(getSupportFragmentManager(),"Notice");
-        }
-        return  super.onOptionsItemSelected(item);
-    }
 }
